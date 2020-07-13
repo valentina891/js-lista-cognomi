2f5cf791c248eef37b041c54cf3d1e3f90a6f9e7
vale <valemil.vs@gmail.com>
2020-07-13 17:50:04 +0200

    passaggi 1-3

--- js/myscript.js
+++ js/myscript.js
@@ -2,3 +2,17 @@
 // inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
+// Passaggi intermedi:
+//
+// 1. creare array di cognomi:
+var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+// 2. chiedi cognome:
+var cognome = prompt("Inserisci il tuo cognome");
+
+// 3. inserire cognome nell'array:
+cognomi.push(cognome);
+console.log(cognomi)
+
+// 4.
