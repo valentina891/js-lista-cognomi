b40126ee36eb6ea754c8897aa87b28e98bd61dae
vale <valemil.vs@gmail.com>
2020-07-13 17:40:30 +0200

    Init

---
+++ js/myscript.js
@@ -0,0 +1,4 @@
+// Chiedere all’utente il cognome
+// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
